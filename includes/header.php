57081495ebc217bdc7e5e251af14198bd095e043
VMaheshwari16 <131899919+VMaheshwari16@users.noreply.github.com>
2026-01-30 12:56:44 +0530

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Patient Visit Manager</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body class="bg-light">
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">
+        <a class="navbar-brand fw-semibold" href="http://localhost/PHPTasks/Patient_Visit_FollowUp/index.php">
+            Patient Visit Manager
+        </a>
+    </div>
+</nav>
+
+<div class="container mt-4">
