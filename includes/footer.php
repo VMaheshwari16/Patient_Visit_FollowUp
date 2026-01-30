5c124b0104a3e3f205e8a2b3c273f5bd433ecef5
VMaheshwari16 <131899919+VMaheshwari16@users.noreply.github.com>
2026-01-30 12:57:08 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+</div> <!-- container -->
+
+<footer class="bg-white text-center text-muted py-3 mt-5 border-top">
+    Healthcare Mini System
+</footer>
+
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
